d58f8b6935d3c226da80558cbca2f56b81373265
Logan Urfer <logan@Logans-MBP.hsd1.az.comcast.net>
2021-11-29 19:49:59 -0700

    Implements connection to oracle DB through JDBC

---
+++ dbConnection.java
@@ -0,0 +1,104 @@
+import java.sql.*;
+
+/**
+* 	Class Name: dbConnection
+*		Author: Logan C. Urfer
+*	
+* Dependencies: java.sql;
+*
+*	   Purpose: This class is responsible for facilitating a connectionto an oracle DB.
+*
+*  Constructor: Parameterized Constructor - Initializes our Connection object to null,
+*				loads the oracle JDBC driver, and saves the login information for the DB
+*				for future use.
+*/
+public class dbConnection {
+	
+	// Private vars
+	private Connection db_conn;
+	// holds the url to the oracle database.
+	private final String url;
+	// holds the username details.
+	private final String user;
+	// holds the password details
+	private final String pass;
+	private Statement stmt;
+
+	/**
+	*	 Name: dbConnection
+	* Purpose: Saves the login information for the oracle DB and loads the oracle JDBC driver
+	*/
+	public dbConnection(String url, String user, String pass) {
+		this.url = url;
+		this.user = user;
+		this.pass = pass;
+		this.db_conn = null;
+		this.stmt = null;
+		loadOracleJDBCDriver();
+	}
+
+	/**
+	*	 Name: loadOracleJDBCDriver
+	* Purpose: Loads the oracle jdbc drive required to establish a connection. Make sure to set
+	*			your class path. Information is in Prog3.java class
+	*/
+	public void loadOracleJDBCDriver() {
+		try {
+			Class.forName("oracle.jdbc.OracleDriver");
+
+		} catch (ClassNotFoundException e) {
+			System.err.println("*** ClassNotFound Exception: "
+					+ "Unable to load Oracle JDBC Driver. \n");
+			System.exit(-1);
+		}
+
+	}
+
+	/**
+	*	 Name: connect
+	* Purpose: Establishes a connection to the oracle DB using the credentials provided in the
+	*			constructor.
+	*/
+	public void connect() {
+		try {
+			db_conn = DriverManager.getConnection(this.url, this.user, this.pass);
+			this.stmt = db_conn.createStatement();
+		} catch (SQLException e) {
+			System.err.println("*** SQLException: Unable to open JDBC Connection. \n");
+			System.exit(-1);
+		}
+	}
+	
+	/**
+	*	 Name: close
+	* Purpose: Closes a connection to the oracle DB.
+	*/	
+	public void close() {
+		try {
+			db_conn.close();
+		} catch (SQLException e) {
+			System.err.println("*** SQLException: Unable to close JDBC Connection. "
+					+ "Did you establish the connection first? \n");
+		}
+	}
+
+	/**
+	*	 Name: executeQuery
+	*   Param: query A String that represents the query to be run
+	*  Return: resultSet A resultSet containing the results from the above query.
+	*/
+	public ResultSet executeQuery(String query) {
+	
+		ResultSet result = null;
+		try {
+			result = this.stmt.executeQuery(query);
+		} catch (SQLException e) {
+			System.err.println("Unable to execute statement query");
+			System.err.println("\tMessage:   " + e.getMessage());
+        	System.err.println("\tSQLState:  " + e.getSQLState());
+        	System.err.println("\tErrorCode: " + e.getErrorCode());
+			System.exit(-1);
+		}
+		return result;
+	}
+}
