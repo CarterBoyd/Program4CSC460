d688d143091b9082a63b7c283b6f82d5be52bdc1
Logan Cole Urfer <lurfer@cs.arizona.edu>
2021-12-04 18:01:33 -0700

    Added CustomerManipulation class skeleton

---
+++ src/CustomerManipulation.java
@@ -0,0 +1,29 @@
+public class CustomerManipulation {
+	private dbConnection conn;
+	private Scanner user_in;
+	
+	private String addCustomerSQL = "";
+	private String deleteCustomerSQL = "";
+	private String updateCustomerSQL = "";
+
+	public CustomerManipulation(Scanner user_in, dbConnection conn) {
+		this.user_in = user_in;
+		this.conn = conn;
+	}
+
+	public void addCustomer() {
+
+
+	}
+
+	public void deleteCustomer() {
+
+	
+	}
+
+	public void updateCustomer() {
+
+
+	}
+	
+}
